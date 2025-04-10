34092bdc9de65bb80af56d1f3efccdaeace456cd
vivek prasad <vivekprasad8921@gmail.com>
2025-04-10 14:42:52 +0530

    Delete inventory directory

--- inventory/HELP.md
+++
@@ -1,4 +0,0 @@
-Subscribe to TechWealthBuzz-
-https://www.youtube.com/@techwealthbuzz
-https://techwealthbuzz.com
-https://x.com/vivekprasadx
--- inventory/src/main/java/com/example/inventory/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.inventory.repository;
-
-import com.example.inventory.model.InventoryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<InventoryItem, Long> {
-}
